7dfe7cf9c455102c454408f848b935aafe2115f4
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-05-30 20:16:39 +0100

    :sparkles: MongoDB configuration

---
+++ movie-service/src/config/mongodb.ts
@@ -0,0 +1,26 @@
+import { Db, MongoClient } from "mongodb";
+
+let connection: MongoClient | null;
+
+let db: Db | null;
+
+export const connect = (callback: any) => {
+  if (connection) return callback(null, db);
+
+  MongoClient.connect(process.env.DATABASE_NAME ?? "", (err, conn) => {
+    if (err) {
+      return callback(err, null);
+    } else {
+      connection = conn;
+      db = conn.db(process.env.DATABASE_NAME);
+      return callback(null, db);
+    }
+  });
+};
+
+export const disconnect = () => {
+  if (!connection) return true;
+  connection.close();
+  connection = null;
+  return true;
+};
